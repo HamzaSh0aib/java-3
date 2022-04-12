a490148f6947ae838cd74a85fdb10bab72a06c9c
HamzaShoaib <h4hamzashoaib@gmail.com>
2022-04-12 02:26:44 +0100

    My Java Assignment

---
+++ app.js
@@ -0,0 +1,119 @@
+
+
+// ------------------Java Assignment 3------------------
+        //-----------Chap 21 to 25-----------
+
+        //-------question 1----
+
+// var name = prompt("Enter Your First Name ");
+// var last_Name = prompt(" Enter Your Last Name");
+// var full_name = (name+" "+last_Name);
+// console.log("Your full name is "+full_name);
+
+//         //-------question 2----
+// var phone_model = prompt("Enter Your MBL Model");
+// console.log("My favorite Phone is : "+phone_model);
+// console.log("Length of String : "+phone_model.length);
+
+         //-------question 3----
+// var Cname = "Pakistani";
+// console.log("Country Name = "+Cname);
+// console.log("Index of 'n' = "+Cname.indexOf("n"));
+
+         //-------question 4----
+// var name = "Hello World";
+// console.log("Greet  = "+name);
+// console.log("Last index of  'l' = "+name.lastIndexOf("l"));
+
+//          //-------question 5----
+
+// var string = "Pakistani";
+// console.log("Find From = "+string);
+// console.log("Character At index 4 = "+string.charAt(4));
+
+//          //-------question 6----
+
+// var first_Name = prompt("Enter Your First Name");
+// var second_Name = prompt("Enter Your Second Name");
+// var Final = first_Name.concat(" ",second_Name);
+// console.log("Your Full Name is = "+Final);
+
+//          //-------question 7----
+
+// var cityName = "Hyderabad";
+// console.log("City Name is = "+cityName);
+// console.log("After Replacement = "+cityName.replace("Hydera","Islama"));
+
+//          //-------question 8----
+
+// var message = "Ali and Sami are best friends. They play cricket and football together: "
+// console.log("Before Replacement = "+message);
+// console.log("  ")
+// console.log("After Replacement of and = "+message.replace(/and/gi,"&"));
+
+//          //-------question 9----
+
+// var string = "472";
+// console.log("Value = 472 ");
+// console.log("Type = "+typeof(string));
+// console.log("")
+// var Number = 472 ;
+// console.log("Value = 472 ");
+// console.log("Type = "+typeof(Number));
+
+//          //-------question 10------
+
+// var Name = prompt("Enter Your Name");
+// console.log("Your Name is = "+ Name);
+// console.log("Your Name in UpperCase = "+Name.toUpperCase());
+
+//          //-------question 11------
+
+// var  fname = prompt("Enter Name");
+// console.log("User Input = "+fname);
+// var convert = fname.charAt(0).toUpperCase()+fname.slice(1);
+// console.log("Title Case = "+ convert);
+
+//          //-------question 12------
+
+// const str = prompt("Enter number");
+// const dotsRemoved = str.replaceAll('.', "");
+// console.log(dotsRemoved);
+
+//          //--------question 13--------
+// var str = prompt("Enter Your Name: ");
+// document.write("Hello"+str)
+// var split = str.split("");
+// var ismatch = false;
+// for(var i = 0;i<split.length;i++){
+//     var char = str.charCodeAt(i);
+
+//     if((char>0 && char<=47)||(char>=58 && char<=64)||(char>=91 && char<=96)||(char>=123 && char<=127)){
+//         alert("Please Enter Valid Name");
+//         ismatch = true;
+//         break
+//     }
+// }
+// if(ismatch===false){
+//     alert("Please enter valid Name")
+// }
+
+
+
+
+/// ///--------question 14
+// var fruits = ["cake","applepie","cookie","chips","patties"];
+// var search = prompt("enter Fruit name");
+// var check = search.toLowerCase();
+// var ismatch = false;
+// for(var i =0;i<fruits.length;i++){
+//     if(check==fruits[i]){
+//         alert(check+"  is Avaliable at index "+i+" in our bakery")
+//        ismatch = true;
+//        break
+//     }
+// }
+// if(ismatch===false){
+//     alert("We Are Sorry!  pastry is not avaliable in our bakery")
+// }
+
